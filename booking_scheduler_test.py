dec5eef270ec027b8ac3a9e16e49aa9bacf5c815
inpyo <winsowss@hanmail.net>
2024-07-11 11:40:59 +0900

    Move testable_sms_sender into setup func

--- booking_scheduler_test.py
+++ booking_scheduler_test.py
@@ -18,6 +18,9 @@ class BookingSchedulerTest(unittest.TestCase):
     def setUp(self):
         print("Start Test")
         self.booking_scheduler = BookingScheduler(CAPACITY_PER_HOUR)
+        self.testable_sms_sender = TestableSmsSender()
+        self.booking_scheduler.set_sms_sender(self.testable_sms_sender)
+
 
     def tearDown(self):
         print("End Test")
@@ -26,7 +29,6 @@ class BookingSchedulerTest(unittest.TestCase):
     def test_예약은_정시에만_가능하다_정시가_아닌경우_예약불가(self):
         # arrange
         schedule = Schedule(NOT_ON_THE_HOUR, UNDER_CAPACITY, CUSTOMER)
-        self.booking_scheduler = BookingScheduler(CAPACITY_PER_HOUR)
 
         # act and assert
         with self.assertRaises(ValueError):
@@ -35,7 +37,6 @@ class BookingSchedulerTest(unittest.TestCase):
     def test_예약은_정시에만_가능하다_정시인_경우_예약가능(self):
         # arrange
         schedule = Schedule(ON_THE_HOUR, UNDER_CAPACITY, Customer("Fake name", "010-1234-5678"))
-        self.booking_scheduler = BookingScheduler(CAPACITY_PER_HOUR)
 
         # act
         self.booking_scheduler.add_schedule(schedule)
@@ -53,7 +54,7 @@ class BookingSchedulerTest(unittest.TestCase):
             self.booking_scheduler.add_schedule(new_schedule)
 
         # assert
-        self.assertEqual("Number of people is over restaurant capacity per hour",str(context.exception))
+        self.assertEqual("Number of people is over restaurant capacity per hour", str(context.exception))
 
     def test_시간대별_인원제한이_있다_같은_시간대가_다르면_Capacity_차있어도_스케쥴_추가_성공(self):
         # arrange
@@ -70,15 +71,13 @@ class BookingSchedulerTest(unittest.TestCase):
 
     def test_예약완료시_SMS는_무조건_발송(self):
         # arrange
-        testable_sms_sender = TestableSmsSender()
         schedule = Schedule(ON_THE_HOUR, UNDER_CAPACITY, CUSTOMER)
-        self.booking_scheduler.set_sms_sender(testable_sms_sender)
 
         # act
         self.booking_scheduler.add_schedule(schedule)
 
         # assert
-        self.assertTrue(testable_sms_sender.is_send_method_is_called())
+        self.assertTrue(self.testable_sms_sender.is_send_method_is_called())
 
     def test_이메일이_없는_경우에는_이메일_미발송(self):
         pass
